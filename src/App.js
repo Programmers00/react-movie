f92666c35cc1c42604080e1206f72053d07ef4be
eunshinNoh <nes0410@gmail.com>
2022-12-15 09:25:13 -0700

    refs #2 To Do List Practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import {useState} from 'react'
+
 function App() {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const onChange = (event) => setTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo === "" || todos.find(item => item === todo)) return
+    setTodos(currentArray => [todo, ...currentArray])
+    setTodo("")
+  }
+  const rmTodo = (event) => {
+    event.preventDefault();
+    // console.log("##event", event)
+    setTodos(currentArray => [...currentArray.filter(element => element !== event.target.innerText)])
+  }
+  // console.log("todos:", todos)
   return (
     <div>
-      react-movie
+      <h1>To Do List ({todos.length})</h1>
+      <h3>When you done, click the To Do for being done</h3>
+      <form onSubmit={onSubmit}>
+        <input value={ todo } onChange={ onChange }type="text" placeholder="Write Your Todo">
+      </input>
+        <button>Submit</button>
+      </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => <li onClick={ rmTodo } key={index}> { item }</li>)}
+      </ul>
+      
     </div>
   );
 }
