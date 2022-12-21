bd4410b1c3fc61a5babdc944d872b19d9c40f123
eunshinNoh <nes0410@gmail.com>
2022-12-20 22:35:22 -0700

    refs #9 deployment setting for gh-pages, github pages

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Detail from "./routes/Detail"
 
 function App() {
   return(
-    <Router>
-      <Routes>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
